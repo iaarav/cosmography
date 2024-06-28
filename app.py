d4141bec4f240ee31f0565c7d83e2a17911a0f73
iaarav <aarav2008bansal@gmail.com>
2024-06-28 23:17:24 +0530

    Commit 1

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Yo"
 
 
 if __name__ == '__main__':
