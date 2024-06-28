3233d8250f6bfff283e679358e1c7f9fcb4c889b
iaarav <aarav2008bansal@gmail.com>
2024-06-29 01:05:29 +0530

    Commit 1

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "Yo, you're cool if you're seeing this"
+@app.route('/get-json/')
+def get_json_data():
+    with open('data.json', mode="r") as file:
+        data = json.load(file)
+
+    return jsonify(data)
+
+
+@app.route('/add-json-data/', methods=['POST'])
+def add_data():
+    new_data = request.get_json()
+    print("Received data:", new_data)  # Debug statement
+
+    try:
+        with open('data.json', mode="r") as file:
+            data = json.load(file)
+        print("Current data in file:", data)  # Debug statement
+    except json.JSONDecodeError as e:
+        print("Error reading JSON file:", e)
+        data = []
+
+    data.append(new_data)
+    print("Updated data:", data)  # Debug statement
+
+    with open('data.json', mode="w") as file:
+        json.dump(data, file, indent=4)
+
+    return jsonify(new_data), 201
 
 
 if __name__ == '__main__':
